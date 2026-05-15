747b24b52568b49166e62031b10472a79ad004f0
AlfioSaitta <alfio@collateral-studios.com>
2026-05-15 22:48:49 +0200

    refactor(android): remove Picture-in-Picture functionality and related code

--- android/app/src/main/java/com/streamai/iptv/MainActivity.java
+++ android/app/src/main/java/com/streamai/iptv/MainActivity.java
@@ -1,11 +1,9 @@
 package com.streamai.iptv;
 
-import android.app.PictureInPictureParams;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Rational;
 import android.view.WindowManager;
 
 import androidx.core.view.WindowCompat;
@@ -15,13 +13,10 @@ import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-	private static final Rational STREAMAI_PIP_ASPECT_RATIO = new Rational(16, 9);
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		applyDisplayCutoutMode();
-		updatePictureInPictureParams();
 		enableImmersiveMode();
 	}
 
@@ -94,43 +89,11 @@ public class MainActivity extends BridgeActivity {
 	}
 
 	@Override
-	protected void onUserLeaveHint() {
-		super.onUserLeaveHint();
-
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !isInPictureInPictureMode()) {
-			try {
-				enterPictureInPictureMode(buildPictureInPictureParams());
-			} catch (IllegalStateException ignored) {
-				// PiP può essere rifiutato dal sistema se l'activity non è in stato valido.
-			}
-		}
-	}
-
-	@Override
 	public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
 		super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
-		updatePictureInPictureParams();
 		// Quando si esce dal PiP rientriamo in immersive landscape.
 		if (!isInPictureInPictureMode) {
 			enableImmersiveMode();
 		}
-	}
-
-	private void updatePictureInPictureParams() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			setPictureInPictureParams(buildPictureInPictureParams());
-		}
-	}
-
-	private PictureInPictureParams buildPictureInPictureParams() {
-		PictureInPictureParams.Builder builder = new PictureInPictureParams.Builder()
-				.setAspectRatio(STREAMAI_PIP_ASPECT_RATIO);
-
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-			builder.setAutoEnterEnabled(true);
-			builder.setSeamlessResizeEnabled(true);
-		}
-
-		return builder.build();
 	}
 }
